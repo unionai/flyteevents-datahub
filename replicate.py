dd546c18ded9038a907fbfae16e66519388f7076
Ketan Umare <ketan.umare@gmail.com>
2021-12-04 22:30:17 -0800

    Better pipelining
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- replicate.py
+++ replicate.py
@@ -1,23 +1,20 @@
-import functools
-import typing
-
 import boto3
 import botocore
 import typer
 
 
-class SQSSource:
+class SQSSource(object):
 
     def __init__(self, name: str):
         self._name = name
         self._sqs: botocore.client.SQS = boto3.client("sqs")
+        response = self._sqs.get_queue_url(QueueName=name)
+        self._queue_url = response['QueueUrl']
 
-    def start_reading(self, callback: typing.Callable):
-        queue_url = 'SQS_QUEUE_URL'
-
+    def read(self):
         # Receive message from SQS queue
         response = self._sqs.receive_message(
-            QueueUrl=queue_url,
+            QueueUrl=self._queue_url,
             AttributeNames=[
                 'SentTimestamp'
             ],
@@ -26,24 +23,20 @@ class SQSSource:
                 'All'
             ],
             VisibilityTimeout=30,
-            WaitTimeSeconds=0
+            WaitTimeSeconds=20,
         )
 
         message = response['Messages'][0]
         receipt_handle = message['ReceiptHandle']
+        return message, receipt_handle
 
-        try:
-            callback(message)
-
-            # Delete received message from queue
-            self._sqs.delete_message(
-                QueueUrl=queue_url,
-                ReceiptHandle=receipt_handle
-            )
-            print(f"Received and deleted message: {message}")
-        except Exception as e:
-            print(f"Received exception, {e}")
-            raise
+    def complete(self, receipt_handle):
+        # Delete received message from queue
+        self._sqs.delete_message(
+            QueueUrl=self._queue_url,
+            ReceiptHandle=receipt_handle
+        )
+        print(f"Received and deleted message: {receipt_handle}")
 
 
 class DataHubSink(object):
@@ -55,16 +48,29 @@ class DataHubSink(object):
         pass
 
 
-def transformer(message: bytes, sink: DataHubSink):
-    # transform
-    transformed = message
-    sink.write(transformed)
+class Pipeline(object):
+
+    def __init__(self, source: SQSSource, sink: DataHubSink):
+        self._source = source
+        self._sink = sink
+
+    def start(self):
+        while True:
+            try:
+                message, handle = self._source.read()
+                # transform
+                transformed = message
+                self._sink.write(transformed)
+                self._source.complete(handle)
+            except Exception as e:
+                pass
 
 
-def main(queue_name: str):
+def main(queue_name: typer.Option("", help="Name of the queue")):
     source = SQSSource(name=queue_name)
     sink = DataHubSink()
-    source.start_reading(functools.partial(transformer, sink=sink))
+    p = Pipeline(source=source, sink=sink)
+    p.start()
 
 
 if __name__ == "__main__":
